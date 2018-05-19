387f5d6258039bb130315ffb39d571ad6250d436
gschlanziboy <dominik.grasser96@gmx.at>
2018-05-20 01:46:54 +0200

    choose image and draw to screen

--- src/at/jku/Main.java
+++ src/at/jku/Main.java
@@ -1,8 +1,49 @@
 package at.jku;
 
+import at.jku.misc.ImageChooser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Main {
 
+    private static JFrame frame;
+    private static ImageChooser imageChooser = new ImageChooser();
+
+
     public static void main(String[] args) {
-	// write your code here
+
+
+
+        SwingUtilities.invokeLater(() -> {
+            frame = createFrame();
+            frame.pack();
+            frame.setLocation(200, 200);
+            frame.setVisible(true);
+        });
     }
-}
+
+
+        private static JFrame createFrame(){
+            JFrame frame = new JFrame("73");
+            frame.getContentPane().setLayout(new BorderLayout());
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+           BufferedImage image = imageChooser.chooseImage(frame);
+
+           JPanel panel = new JPanel();
+           frame.getContentPane().add(panel,BorderLayout.NORTH);
+
+           JLabel label = new JLabel(new ImageIcon(image));
+           panel.add(label);
+
+            return  frame;
+    }
+
+
+
+
+    }
+
---
+++ src/at/jku/misc/ImageChooser.java
@@ -0,0 +1,39 @@
+package at.jku.misc;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageChooser {
+
+
+
+    public BufferedImage chooseImage(JFrame frame){
+
+        JFileChooser jfc = new JFileChooser();
+        jfc.setDialogTitle("Please choose your base image");
+        jfc.setFileFilter(new FileNameExtensionFilter("jpg or png images","jpg","png"));
+        int dialogValue = jfc.showOpenDialog(frame);
+
+        File file;
+        BufferedImage image = null;
+
+        if (dialogValue == JFileChooser.APPROVE_OPTION){
+            file = jfc.getSelectedFile();
+
+            try {
+                image = ImageIO.read(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        return image;
+    }
+
+}
